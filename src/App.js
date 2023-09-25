7fd1a0a28209f116e7153cab91e8b72ec279bf39
chiragbansal10 <98810248+chiragbansal10@users.noreply.github.com>
2023-09-25 15:24:04 +0530

    search first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import BeerList from "./components/BeerList";
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    async function fetchBeers() {
+      try {
+        const response = await fetch("https://api.punkapi.com/v2/beers");
+        const data = await response.json();
+        setBeers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchBeers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Search beers"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <BeerList beers={beers} />
     </div>
   );
 }
